401b262e13974bddfa1bfc33b48c8116e3ddc8ee
parita15 <parishah052@gmail.com>
2022-11-02 13:11:22 -0400

    URI changes

--- server/config/db.js
+++ server/config/db.js
@@ -2,5 +2,5 @@ module.exports = {
   //local MongoDB deployment ->
   // "URI": "mongodb://127.0.0.1:27017/midterm"
   // "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority"
-  "URI": "mongodb+srv://pari:Parita@1502@cluster0.ffexnhk.mongodb.net/midterm?retryWrites=true&w=majority"
+  "URI": "mongodb+srv://pari:Parita@1502@mongodbserver.ffexnhk.azure.mongodb.net/midterm?retryWrites=true&w=majority"
 };
