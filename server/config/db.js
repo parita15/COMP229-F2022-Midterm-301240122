d9b9d400887347323057d026248818165ac9b367
parita15 <parishah052@gmail.com>
2022-11-02 12:38:22 -0400

    uri changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://127.0.0.1:27017/midterm"
+  // "URI": "mongodb://127.0.0.1:27017/midterm"
+  "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority"
 };
