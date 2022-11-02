c4cec2b839c9f2250a09f989c78d888b02d7c89e
parita15 <parishah052@gmail.com>
2022-11-02 15:14:05 -0400

    made some changes

--- server/config/db.js
+++ server/config/db.js
@@ -3,5 +3,5 @@ module.exports = {
   // "URI": "mongodb://127.0.0.1:27017/midterm"
   // "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority"
  "URI": "mongodb+srv://pari:Parita@1502@cluster0.ffexnhk.mongodb.net/midterm?retryWrites=true&w=majority"
-  // "URI": "mongodb+srv://pari:Parita@1502@cluster0.ffexnhk.mongodb.net/?retryWrites=true&w=majority"
+// "URI":"mongodb+srv://cencollege:6GX6OmfUtvvclKT4@cluster0.vikkbvp.mongodb.net/?retryWrites=true&w=majority"
 };
